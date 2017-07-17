d581edce6ecaf273233a1d5df565f6676034d443
Dan Beaulieu <danbeaulieu11@gmail.com>
2017-07-17 12:06:06 -0500

    added HTML label renderers

---
+++ Droid Renderers/HTMLLabelRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget; 
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(HtmlLabel), typeof(HtmlLabelRenderer))]
+namespace CodeBeaulieu.Droid
+{  
+	public class HtmlLabelRenderer : LabelRenderer
+	{ 
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+
+			Control?.SetText(Html.FromHtml(Element.Text), TextView.BufferType.Spannable);
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == Label.TextProperty.PropertyName)
+			{
+				Control?.SetText(Html.FromHtml(Element.Text), TextView.BufferType.Spannable);
+			}
+		}
+	} 
+}
---
+++ iOS Renderers/HTMLLabelRenderer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Foundation; 
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HtmlLabel), typeof(HtmlLabelRenderer))]
+namespace CodeBeaulieu.iOS
+{  
+	public class HtmlLabelRenderer : LabelRenderer
+	{
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null && Element != null && !string.IsNullOrWhiteSpace(Element.Text))
+			{
+				var attr = new NSAttributedStringDocumentAttributes();
+				var nsError = new NSError();
+				attr.DocumentType = NSDocumentType.HTML;
+
+				UIKit.UIFont font = Control.Font;
+				string fontName = font.Name;
+				System.nfloat fontSize = font.PointSize;
+				string htmlContents = "<span style=\"font-family: '" + fontName + "'; font-size: " + fontSize + "\">" + Element.Text + "</span>";
+				var myHtmlData = NSData.FromString(htmlContents, NSStringEncoding.Unicode);
+				Control.Lines = 0;
+				Control.AttributedText = new NSAttributedString(myHtmlData, attr, ref nsError);
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == Label.TextProperty.PropertyName)
+			{
+				if (Control != null && Element != null && !string.IsNullOrWhiteSpace(Element.Text))
+				{
+					var attr = new NSAttributedStringDocumentAttributes();
+					var nsError = new NSError();
+					attr.DocumentType = NSDocumentType.HTML;
+
+					UIKit.UIFont font = Control.Font;
+					string fontName = font.Name;
+					System.nfloat fontSize = font.PointSize;
+					string htmlContents = "<span style=\"font-family: '" + fontName + "'; font-size: " + fontSize + "\">" + Element.Text + "</span>";
+					var myHtmlData = NSData.FromString(htmlContents, NSStringEncoding.Unicode);
+					Control.Lines = 0;
+					Control.AttributedText = new NSAttributedString(myHtmlData, attr, ref nsError);
+				}
+			}
+		}
+	}
+}
